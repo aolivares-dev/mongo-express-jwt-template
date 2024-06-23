27a7e80656f5513a6ca42a77b5b6014dddcb2fd7
ElDuro <vargas.alessandro12@gmail.com>
2024-06-23 10:06:42 -0500

    Refactor database connection to use environment variable

--- models/Model.js
+++ models/Model.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 const Connection = () => {
-	mongoose.connect('mongodb://127.0.0.1:27017/dev-app')
+	mongoose.connect(process.env.DB_URL)
 		.then(() => {
 			console.log("Conexion exitosa");
 		})
--- readme.txt
+++ readme.txt
@@ -1,5 +1,5 @@
 PORT=3000
-DB_URL=mongodb://localhost:27017/app-dev
+DB_URL=mongodb://127.0.0.1:27017/dev-app
 API_KEY=mlBo7sROxF7bJvveaz7gW2wfUGlWY6YgTDSs2ooKeag=
 
 # convertir este archivo a extension .env
